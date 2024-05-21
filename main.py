ff3d67f3252803c4b217f62b3f7add911c1a300c
Enrique mota <enrique.motadejesus@gmail.com>
2024-05-21 09:41:46 -0400

    turtle library

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+john = Turtle()
+john.shape('turtle')
+john.color('blue4')
+john.forward(100)
+print(john)
+
+
+my_screen = Screen()
+print({my_screen.canvheight})
+my_screen.exitonclick()
