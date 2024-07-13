6d8976a54831eca0f49cfff66a6262c320a91424
Enrique mota <enrique.motadejesus@gmail.com>
2024-07-13 11:06:56 -0400

    Updates

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-john = Turtle()
-john.shape('turtle')
-john.color('blue4')
-john.forward(100)
-print(john)
+# john = Turtle()
+# john.shape('turtle')
+# john.color('blue4')
+# john.forward(100)
+# print(john)
 
 
-my_screen = Screen()
-print({my_screen.canvheight})
-my_screen.exitonclick()
+# my_screen = Screen()
+# print({my_screen.canvheight})
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+print(table)
